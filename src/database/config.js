9a4c3a44c79f5202fa1565900cbe419f05137d75
luchojm96 <luchojm96@gmail.com>
2021-04-04 13:05:35 -0400

    connection fix

--- src/database/config.js
+++ src/database/config.js
@@ -1,22 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.USER,
-  process.env.PASSWORD,
-  {
-    host: process.env.HOST,
-    dialect: 'postgres',
-  }
-);
+const sequelize = new Sequelize({
+  database: process.env.DB,
+  host: process.env.HOST,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  port: 5432,
+  dialect: 'postgres',
+});
 
 const dbConnection = async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error('Unable to connect to the database:', error.message);
   }
-}
+};
 
 module.exports = { dbConnection, sequelize };
